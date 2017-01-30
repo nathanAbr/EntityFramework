75d14c3a2e54320c0a723078ede8ae3049bc7fd6
nathanABR <nathan.abraham@outlook.fr>
2017-01-30 10:14:37 +0100

    Insert blog in db

--- entityFramework/Program.cs
+++ entityFramework/Program.cs
@@ -11,6 +11,30 @@ namespace entityFramework
     {
         static void Main(string[] args)
         {
+            using (var db = new BloggingContext())
+            {
+                // Create and save a new Blog 
+                Console.Write("Entrez un nom de blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database 
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("Tous les blogs :");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
 
