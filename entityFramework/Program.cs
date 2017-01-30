eaa9f33584b44c9e205c938112fa6b2b33c7f5dd
nathanABR <nathan.abraham@outlook.fr>
2017-01-30 09:57:49 +0100

    First commit to EntityFramework exercise

--- entityFramework/Program.cs
+++ entityFramework/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace entityFramework
 {
@@ -11,5 +12,36 @@ namespace entityFramework
         static void Main(string[] args)
         {
         }
+    }
+
+    public class Tag
+    {
+        public String Name { get; set; }
+        public int TagId { get; set; }
+        public List<Post> Post { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+        public List<Tag> Tag { get; set; }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
     }
 }
